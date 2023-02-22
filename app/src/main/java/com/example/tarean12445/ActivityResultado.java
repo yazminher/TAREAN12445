76de166bec69b2d8259dcd1cf5f7f0960852f5e4
yazminher <119539077+yazminher@users.noreply.github.com>
2023-02-22 09:32:01 -0800

    Initial commit

---
+++ app/src/main/java/com/example/tarean12445/ActivityResultado.java
@@ -0,0 +1,14 @@
+package com.example.tarean12445;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActivityResultado extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_resultado);
+    }
+}
